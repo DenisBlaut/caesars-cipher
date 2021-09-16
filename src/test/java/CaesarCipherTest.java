396e9e466d4ad8ce1a15a8243e0a9b30e319b231
Denis Blaut <denis.blaut@fortis-it.de>
2021-09-16 09:23:16 +0200

    Initial commit

---
+++ src/test/java/CaesarCipherTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarCipherTest {
+
+    CaesarsCipher caesarsCipher = new CaesarsCipher();
+
+    @Test
+    void testCipheredMessageWithOffsetOf12() {
+        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+    }
+
+    @Test
+    void testEmptyString() {
+        assertEquals("", caesarsCipher.cipher("", 12));
+    }
+}
